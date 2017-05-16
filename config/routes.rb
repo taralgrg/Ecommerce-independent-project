2be4deff89d0071112404abb54066faa6219ba9b
Mac <taralgurung@hotmail.com>
2017-05-16 14:20:41 +0800

    shopping cart feature added, functions well

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :order_items
   resources :orders
   resources :products
+  resource  :cart, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
