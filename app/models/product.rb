40fb13c82a9bf2473a8be630e8023e36027b10ab
Mac <taralgurung@hotmail.com>
2017-05-18 09:37:03 +0800

    added login/logout, index page directed to sign up

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
   has_many :order_items, dependent: :destroy
   has_attached_file :image, styles: { large: "600x600", medium: "300x300", thumb: "150x150#"}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  belongs_to :user
   # has_many :materials, inverse_of: :product, dependent: :destroy
   # accepts_nested_attributes_for :materials, reject_if: :all_blank, allow_destroy: true
   # has_many :ingredients, inverse_of: :product, dependent: :destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :order_items
   resources :orders
   resources :products
   resource  :cart, only: [:show]
   resources :charges
+  root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
